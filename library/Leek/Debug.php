49dea73385204ef4c8e8efe89ede5f65fceea82e
leek <leeked@gmail.com>
2011-07-07 03:11:55 -0400

    var_dump() debugging FTW

--- library/Leek/Debug.php
+++ library/Leek/Debug.php
@@ -21,16 +21,54 @@ class Leek_Debug
 {
     public static function dump($thing)
     {
+        // In case we are dumping inside of these
+        echo '</script></style><div style="border: 2px solid red; background-color: white; padding: 5px">';
+        
+        $backtrace = debug_backtrace();
+        foreach ($backtrace as $trace) {
+            if ($trace['file'] != __FILE__) {
+                if ($trace['function'] == 'dd' || $trace['function'] == 'd') {
+                    echo "<pre style=\"font-size: 85%\">Line <b>{$trace['line']}</b> of <b>{$trace['file']}</b>:</pre>";
+                }
+            }
+        }
+
         if (extension_loaded('xdebug')) {
-            return var_dump($thing);
+            var_dump($thing);
         } else {
             if (class_exists('krumo', false)) {
-                return krumo::dump($thing);
+                krumo::dump($thing);
             } else {
                 echo '<pre>';
                 var_dump($thing);
                 echo '</pre>';
             }
         }
+        
+        echo '</div><br>';
+    }
+}
+
+if (function_exists('d') === FALSE) {
+    /**
+     * Dump whichever way we can.
+     *
+     * @param mixed $thing
+     * @return string
+     */
+    function d($thing) {
+        Leek_Debug::dump($thing);
+    }
+}
+
+if (function_exists('dd') === FALSE) {
+    /**
+     * Dump and die
+     *
+     * @see d()
+     * @param mixed $thing
+     */
+    function dd($thing) {
+        d($thing); exit;
     }
 }
